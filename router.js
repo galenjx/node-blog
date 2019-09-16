a59c6e423707649aba8a2a95cc169b1e90cf97dd
galenjx <galenjx@163.com>
2019-09-16 13:59:55 +0800

    完成多表联合查询渲染，整理页面

--- router.js
+++ router.js
@@ -4,7 +4,8 @@ var Post = require('./models/post')
 var Comment = require('./models/comment')
 var md5 = require('blueimp-md5')
 var marked=require('marked')
-var fs=require('fs')
+var mongoose=require('mongoose')
+// var fs=require('fs')
 var router = express.Router()
 
 
@@ -12,10 +13,40 @@ var router = express.Router()
 
 //+=====================================首页===========================================
 router.get('/', function (req, res) {
-  // console.log(req.session.user)
-  res.render('index.html', {
-    user: req.session.user
+
+  Post.aggregate([
+    {
+      $lookup: {
+        from: "users",
+        localField: "author_id",
+        foreignField: "_id",
+        as: "user_detail"
+      }
+    },
+    {
+      $lookup: {
+        from: "comments",
+        localField: "_id",
+        foreignField: "post_id",
+        as: "comment_detail"
+      }
+    }
+  ],function(err,result){
+    if (err) {
+      // 交给处理错误的中间件
+      return next(err)
+    }
+    res.render('index.html', {
+      user: req.session.user,
+      lists: result
+    })
   })
+  .sort({_id:-1})
+  .limit(20)
+
+
+
+  
 })
 
 
@@ -160,7 +191,7 @@ router.post('/posts/new', function (req, res, next) {
     message: '请填写完整的文章信息'
   })
   body.author_id=user._id
-  console.log(body)
+  // console.log(body)
   new Post(body).save(function (err, post) {
     if (err) {
       return next(err)
@@ -188,25 +219,91 @@ router.get('/posts/show', function (req, res, next) {
   // 1. 获取要渲染的文章id
   // 
   // 2. 渲染编辑页面
-  //    根据 id 把文章信息查出来
+  //    根据 id 把文章信息,评论内容查出来，
+  //    =======注意获取的id有双引号，而且查询时id类型要为ObjectId，这个问题困住我一大个早上==============
   //    使用模板引擎渲染页面
 
-  //进来先清空文章的session
-  req.session.post=''
 
-  Post.findById(req.query.id, function (err, post) {
+  //views+1
+    // var newPost=null
+    // newPost.views = result[0].views + 1
+    // new Post(newPost).save(function (err, user) {
+    //   if (err) {
+    //     return next(err)
+    //   }
+      
+    //   res.status(200).json({
+    //     err_code: 0,
+    //     message: 'OK'
+    //   })
+  
+    // })
+
+
+
+  req.session.post=''
+  var id = req.query.id.replace(/"/g,'')
+  Oid = mongoose.Types.ObjectId(id);
+  // console.log(Oid)
+  Post.aggregate([
+    {
+      $lookup: {
+        from: "users",
+        localField: "author_id",
+        foreignField: "_id",
+        as: "user_detail"
+      }
+    },
+    {
+      $lookup: {
+        from: "comments",
+        localField: "_id",
+        foreignField: "post_id",
+        as: "comment_detail"
+      }
+    },
+    {
+       $match : { _id : { $eq : Oid }}
+     }
+  ],function(err,result){
     if (err) {
+      // 交给处理错误的中间件
       return next(err)
     }
+    req.session.post=result[0]
+    // console.log(result[0].content)
+    result[0].content=marked(result[0].content)
 
-    req.session.post=post
 
-    posthtml=marked(post.content)
+
+    // console.log(result[0])
     res.render('topic/show.html', {
-      user:req.session.user,
-      post:posthtml
+      user: req.session.user,
+      result: result[0],
+      post: req.session.post
     })
   })
+
+
+  //进来先清空文章的session
+  // req.session.post=''
+  // console.log(req.query.id)
+  // Post.findById(req.query.id, function (err, post) {
+  //   if (err) {
+  //     return next(err)
+  //   }
+
+  //   // req.session.post=post
+
+  //   // posthtml=marked(post.content)
+  //   // res.render('topic/show.html', {
+  //   //   user:req.session.user,
+  //   //   post:posthtml
+  //   // })
+  // })
+
+
+
 })
 
 
@@ -228,8 +325,9 @@ router.post('/posts/comment', function (req, res, next) {
     message: '评论不能为空'
   })
   body.author_id=user._id
+  body.author_nickname=user.nickname
   body.post_id=post_S._id
-  console.log(body)
+  // console.log(body)
   new Comment(body).save(function (err, user) {
     if (err) {
       return next(err)
